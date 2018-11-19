1fd9e2bb6f6ae3776f4802de7ccf084439a3c2d5
Boyko Vodenicharski <boyko.vodenicharski@gmail.com>
2018-11-19 21:18:20 +0000

    Add files
    record_frane.m not yet working

---
+++ gridscan.m
@@ -0,0 +1,46 @@
+fid = fopen('rbc_tile_test.xml', 'wt');
+
+fprintf(fid, '<temika>\n');
+
+% Define grid area
+x1 = [0,250];
+x2 = [0,750];
+x3 = [-10500,250];
+x4 = [-10500,750];
+fov_x = 295; % x FoV in microns absolute value
+fov_y = 250; % y Fov in microns absolute value
+led_intensity = 0.01;
+overlap_x = 10; % number of microns to overlap adjacent fields of view
+overlap_y = 10;
+
+diagonal = x4 - x1;
+% +1 to make sure entire area is scanned because floor rounds to neg infinity and part of the area is lost
+x_steps = abs(floor(diagonal(1) / fov_x)) + 1;
+y_steps = abs(floor(diagonal(2) / fov_y)) + 1;
+
+% Letting step in x and y to be one FoV
+delta_x = fov_x * sign(diagonal(1));
+delta_y = fov_y * sign(diagonal(2));
+
+% Correction for keeping focus in case PFS does not work properly.
+% Take change in z between x0 and x0+diagonal and apply gradient.
+z_correction_y = 0; % microns correction in z per step in y direction
+z_correction_x = 0; % microns correction in z per step in x direction
+
+x_positions = linspace(x1(1), x_steps * delta_x, x_steps);
+y_positions = linspace(y_steps * delta_y, x1(2), y_steps); % Initialise flipped so that scan moves in the right direction in the for loop
+
+for ii=1:x_steps
+    y_positions = fliplr(y_positions); % This is to make the scan not return all the way back and waste time.
+    for jj=1:y_steps
+        move_absolute(fid, x_positions(ii), y_positions(jj));
+        sleep(fid,0.2);
+        basename = strcat(pwd,['/X_',num2str(x_positions(ii))],['.Y_',num2str(y_positions(jj)),'.']);
+        record(fid, 3, basename);
+    end
+end
+        
+
+set_illumination(fid, 3, 0.01, 'OFF');
+fprintf(fid, '</temika>');
+fclose(fid);
---
+++ record_frame.m
@@ -0,0 +1,34 @@
+function record_frame(fid, basename)
+%INPUT: file identifier, basename for the file
+%OUTPUT: none, writes xml for taking a single photo for an iidc camera
+
+fprintf(fid, '<camera name="IIDC Point Grey Research Grasshopper3 GS3-U3-23S6M">\n');
+fprintf(fid, '<transmission>ON</transmission>');
+
+fprintf(fid, '\t<iidc>\n');
+fprintf(fid, '\t\t<video_mode>0</video_mode>\n');
+fprintf(fid, '\t\t<color_coding>RAW8</color_coding>\n');
+fprintf(fid, '\t\t<size>1920 1200 0 0</size>\n');
+fprintf(fid, '\t\t<packet_size>14688</packet_size>\n');
+fprintf(fid, '\t\t<brightness mode="relative">120</brightness>\n');
+fprintf(fid, '\t\t<exposure mode="off"></exposure>\n');
+fprintf(fid, '\t\t<gamma mode="off"></gamma>\n');
+fprintf(fid, '\t\t<gain mode="relative">0</gain>\n');
+fprintf(fid, '\t\t<trigger_delay mode="off"></trigger_delay>\n');
+fprintf(fid, '\t\t<shutter mode="relative">1244</shutter>\n');
+fprintf(fid, '\t\t<packet_size>7576</packet_size>\n');
+
+fprintf(fid, '\t</iidc>\n');
+
+fprintf(fid, '\t<trigger>SOFTWARE</trigger>\n');
+fprintf(fid, '\t<trigger_timeout>5.2</trigger_timeout>\n');
+fprintf(fid, '\t<send_trigger></send_trigger>\n');
+
+fprintf(fid, '</camera>\n');
+
+fprintf(fid, '<save>');
+fprintf(fid, strcat('<basename>',basename,'</basename>'));
+fprintf(fid, '<append>TWONUMBERS</append>');
+fprintf(fid, '</save>');
+
+end
