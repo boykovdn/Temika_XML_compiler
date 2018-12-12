4a3d626b3e83081e559812a2cf40776d13ceee0d
Nicola Pellicciotta <np451@cellimager.bss.phy.private.cam.ac.uk>
2018-12-12 17:30:02 +0000

    Update to lysed scan experiment

--- nikon100.m
+++ nikon100.m
@@ -7,9 +7,10 @@ led_intensity = [0.05633, 0.12706, 0.16247];
 basename = '/home/np451/data/boyko/100nikon/'; 
 filename = 'nikon';
 movement_illumination = 0.07365; % LED3 (white)
+num_fovs = 10;
 
-displacement_both = 3; %um in both directions
-num_steps = 100; % resolution of movement
+displacement_both = 4; %um in both directions
+num_steps = 120; % resolution of movement
 
 % Workflow involves changing this number for every cell in the TEM
 % grid, thus being able to more easily work with the images. Using
@@ -17,25 +18,30 @@ num_steps = 100; % resolution of movement
 % different seconds because it takes ~0.3s for the movies to be
 % taken.
 
-index = 14;
-focus_height = 2096.800; % Absolute movement from this position
+index = 29;
+focus_height = 2086.425; % Absolute movement from this position
 %%%
 
 
 fid = fopen(xml_name, 'wt');
 fprintf(fid, '<temika>\n');
-% Z drive
-positions_z = linspace(focus_height - displacement_both, focus_height + displacement_both, num_steps);
 
-for position=positions_z
-    move_z_absolute(fid, position, speed_z_movement);
-    for kk=1:3
-        set_illumination(fid, kk-1, led_intensity(kk), 'ON');
-        sleep(fid, 0.1);
-        record(fid, 0.1, strcat(basename,filename,'_', num2str(index, '%03.f'), '_', 'Z', num2str(position), '_', led_names{kk}));
+
+for ii=1:num_fovs
+    % Z drive
+    positions_z = linspace(focus_height - displacement_both, focus_height + displacement_both, num_steps);
+    for position=positions_z
+        move_z_absolute(fid, position, speed_z_movement);
+        for kk=1:3
+            set_illumination(fid, kk-1, led_intensity(kk), 'ON');
+            sleep(fid, 0.1);
+            record(fid, 0.1, strcat(basename,filename,'_', num2str(index, '%03.f'), '_', 'Z', num2str(position), '_', led_names{kk}));
+        end
     end
+    move_relative(fid, -3.9, 123); 
+    index = index + 1;
+    focus_height = focus_height + 0.6;
 end
-
 set_illumination(fid, 3, movement_illumination, 'ON'); % Keep illumination on so I can quickly move to next location. Colour is arbitrary.
 
 fprintf(fid, '</temika>');
